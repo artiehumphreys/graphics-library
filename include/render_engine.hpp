94b978414036fff09b609b6bc728dfb9357bc49e
artiehumphreys <artieh2003@gmail.com>
2026-02-05 15:22:52 -0500

    feat(render): functions for shape ops with renderer

--- include/render_engine.hpp
+++ include/render_engine.hpp
@@ -3,8 +3,16 @@
 #include "command.hpp"
 #include "spsc_queue.hpp"
 #include "window.hpp"
+#include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <utility>
+
+struct ShapeInstance {
+  uint32_t id;
+  Shape shape;
+  float x, y;
+};
 
 class RenderEngine {
 public:
@@ -29,11 +37,44 @@ public:
       default:
         break;
       }
+      consumer_.pop();
+    }
+    draw();
+  }
+
+  void clear() { shapes_.clear(); }
+
+  void createShape(const CreateData &data) {
+    shapes_.push_back({shape_id++, data.shape, data.x, data.y});
+  }
+
+  void deleteShape(uint32_t id) {
+    auto it = std::find_if(shapes_.begin(), shapes_.end(),
+                           [id](const ShapeInstance &s) { return s.id == id; });
+    if (it != shapes_.end()) {
+      std::swap(*it, shapes_.back());
+      shapes_.pop_back();
     }
   }
+
+  void moveShape(const MoveData &data) {
+    auto it =
+        std::find_if(shapes_.begin(), shapes_.end(),
+                     [&](const ShapeInstance &s) { return s.id == data.id; });
+
+    if (it != shapes_.end()) {
+      it->x = data.x;
+      it->y = data.y;
+    }
+  }
+
+  void draw();
 
 private:
   uint32_t width_, height_;
   std::unique_ptr<Window> window_;
   SPSCQueue<Command>::ConsumerHandle consumer_;
+  // start with naive approach
+  std::vector<ShapeInstance> shapes_;
+  uint32_t shape_id = 0;
 };
